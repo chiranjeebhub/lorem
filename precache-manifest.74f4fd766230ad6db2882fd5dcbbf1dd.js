ce32c48f5ffd242ded73cfe00dbe04315eff24b3
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-04-30 16:49:22 +0530

    Updates

---
+++ precache-manifest.74f4fd766230ad6db2882fd5dcbbf1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c10935b16cbe8acabb6e58f460a0251",
+    "url": "/lorem/index.html"
+  },
+  {
+    "revision": "7a9c839eebca33981965",
+    "url": "/lorem/static/css/main.15108b66.chunk.css"
+  },
+  {
+    "revision": "c67dc58e706ebf19314e",
+    "url": "/lorem/static/js/2.4de14262.chunk.js"
+  },
+  {
+    "revision": "7a9c839eebca33981965",
+    "url": "/lorem/static/js/main.8e9b9d27.chunk.js"
+  },
+  {
+    "revision": "7f5fa15544202646826c",
+    "url": "/lorem/static/js/runtime~main.03c8ed92.js"
+  }
+]);
